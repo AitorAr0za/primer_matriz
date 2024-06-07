6ee6344e9de74783306bcfc9449073e174a8ee6e
Aitor <165178646+AitorAr0za@users.noreply.github.com>
2024-06-07 13:55:56 -0300

    primer_matriz.cpp

--- mayor_en_vector.cpp
+++ mayor_en_vector.cpp
@@ -6,31 +6,69 @@
 #include <cstring>
 #include <string.h>
 #include <cctype>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main() {
 
-    int LARGO = 5;
-    int numeros[LARGO];
-    int mayor;
+    const int FILAS = 3;
+    const int COLUMNAS = 4;
+    int matriz[FILAS][COLUMNAS];
+    int filita;
+    int columnita;
+    int swich = 1;
+    int elemento;
 
-    for (int i = 0; i < LARGO; i++) {
+    srand(time(0));
 
-        int numero;
+    for (int i = 0; i < FILAS; i++) {
 
-        printf("Ingrese un numero de indice %d para insertar en el vector: ", i);
-        scanf(" %d", &numero);
+        printf("| ");
 
-        numeros[i] = numero;
+        for (int j = 0; j < COLUMNAS; j++) {
 
-        if (i == 0) mayor = numeros[i];
+            matriz[i][j] = rand() % 100;
 
-        else if (numeros[i] > mayor) mayor = numeros[i];
+            if (matriz[i][j] < 10) printf("%d  |", matriz[i][j]);
+
+            else printf("%d |", matriz[i][j]);
+        }
+
+        printf("\n");
 
     }
 
-    printf("El mayor de los numeros ingresados es: %d", mayor);
+    printf("\n");
+
+    while (swich) {
+
+        printf("Ingrese la fila del indice que se desea buscar: ");
+        scanf("%d", &filita);
+
+        filita--;
+
+        printf("\nAhora ingrese la columna del indice que se desea buscar: ");
+        scanf("%d", &columnita);
+
+        columnita--;
+
+        if (filita < 0 or columnita < 0) printf("\nHas ingresado un indice negativo, por favor ingrese indices validos.\n\n");
+
+        else if (filita >= FILAS or columnita >= COLUMNAS) printf("\nHas ingresado un indice fuera del rango de la matriz, por favor ingrese indices validos.\n\n");
+
+        else (swich = 0);
+
+    }
+
+    elemento = matriz[filita][columnita];
+
+    if (elemento % 2 == 0) printf("\nEl numero que se encuentra en la fila %d columna %d es el %d. Es un numero par", filita, columnita, elemento);
+
+    else printf("\nEl numero que se encuentra en la fila %d columna %d es el %d. Es un numero impar", filita, columnita, elemento);
+
+    getch();
 
     return 0;
 }
